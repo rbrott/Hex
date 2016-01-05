e1e6e84a9529be1fcc995be35b2af9aac92b28f4
Ryan Brott <rcbrott@gmail.com>
2016-01-04 22:11:14 -0800

    Fix gradle build

--- build/generated/source/r/debug/com/hex/R.java
+++ build/generated/source/r/debug/com/hex/R.java
@@ -19,11 +19,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int metaButtonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f060000;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -46,32 +46,32 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int game=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int dummy_button=0x7f060002;
-        public static final int dummy_content=0x7f060003;
-        public static final int hello_world=0x7f060004;
-        public static final int play_button_text=0x7f060005;
-        public static final int title_activity_game=0x7f060006;
+        public static final int action_settings=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int dummy_button=0x7f070002;
+        public static final int dummy_content=0x7f070003;
+        public static final int hello_world=0x7f070004;
+        public static final int play_button_text=0x7f070005;
+        public static final int title_activity_game=0x7f070006;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
 
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int ButtonBar=0x7f070002;
-        public static final int ButtonBarButton=0x7f070003;
-        public static final int FullscreenActionBarStyle=0x7f070005;
-        public static final int FullscreenTheme=0x7f070004;
+        public static final int AppTheme=0x7f050003;
+        public static final int ButtonBar=0x7f050004;
+        public static final int ButtonBarButton=0x7f050005;
+        public static final int FullscreenActionBarStyle=0x7f050001;
+        public static final int FullscreenTheme=0x7f050002;
     }
     public static final class styleable {
         /** Attributes that can be used with a ButtonBarContainerTheme.
